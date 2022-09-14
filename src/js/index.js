d1ca441c137a103c394aa3dd39ae8e17d08ad984
Fabio <fabiowillian.shr@gmail.com>
2022-09-14 11:49:20 -0300

    Finalizando Projeto

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,14 @@ const ul = document.querySelector('.lista')
 function insereCard() {
     for(let i = 0; i < data.length; i++) {        
         const card = document.createElement('li')
-        card.className = 'cardProduct'
-        
+        card.className = 'cardProduct'        
         const imagem = document.createElement('div')
         const img = document.createElement('img')
         imagem.appendChild(img)
         imagem.className = 'imagem'
         img.src = data[i].img
-        
         const informacoes = document.createElement('div')
         informacoes.className = 'informacoes'
-
         const categoria = document.createElement('button')
         const title = document.createElement('h3')
         const description = document.createElement('p')
@@ -21,32 +18,25 @@ function insereCard() {
         categoria.className = 'categoria'
         addCart.className = 'addCart'
         addCart.id = i+1
-
         categoria.innerText = data[i].tag[0]
         title.innerText = data[i].nameItem
         description.innerText = data[i].description
         price.innerText = `R$ ${data[i].value}.00`
         addCart.innerText = 'Adicionar ao carrinho'
-
         informacoes.appendChild(categoria)
         informacoes.appendChild(title)
         informacoes.appendChild(description)
         informacoes.appendChild(price)
         informacoes.appendChild(addCart)
-
         card.appendChild(imagem)
         card.appendChild(informacoes)
-
         ul.appendChild(card)
     }
 }
-
 insereCard()
 
 const addCart = document.querySelectorAll('.addCart')
-
 const ulCart = document.querySelector('.carrinho')
-
 const precoTotal = document.querySelector('.precoParagrafo')
 const quantidadeTotal = document.querySelector('.quantidadeParagrafo')
 let precoResumo = 0
@@ -63,22 +53,17 @@ function carrinho(){
         const li = document.createElement('li')
         li.className = 'item-cart'
         li.id = item.id
-        
         const img = document.createElement('img')
         img.src = data[item.id - 1].img
-        
         const caracteristicas = document.createElement('div')
         caracteristicas.className = 'caracteristicas'
-        
         const title = document.createElement('h3')
         title.innerText = data[item.id - 1].nameItem
-        
         const preco = document.createElement('h4')
         preco.innerText = `R$ ${data[item.id-1].value}.00`
-        
         const remover = document.createElement('p')
         remover.innerText = 'Remover produto'
-        
+
         caracteristicas.appendChild(title)
         caracteristicas.appendChild(preco)
         caracteristicas.appendChild(remover)
@@ -87,8 +72,7 @@ function carrinho(){
         li.appendChild(caracteristicas)
         
         ulCart.appendChild(li)
-
-        
+ 
         precoTotal.innerHTML = `R$ ${precoResumo}.00`
         console.log(addCart)
 
